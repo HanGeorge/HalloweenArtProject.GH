517df23788e59284e0a2e14332d527ba029bb101
HanGeorge <91705358+HanGeorge@users.noreply.github.com>
2021-11-01 10:46:51 -0700

    Add files via upload

--- #Halloween Art Project.py
+++ #Halloween Art Project.py
@@ -1,37 +1,49 @@
-#Halloween Art Project
-
 import turtle as trtl
 import random as rand
+
+
+
 t=trtl.Turtle()
 
-
-
-
-def turtle_clicked(x, y):
-    change_position()
-   
+gif_list = [ 'skell2.gif', 'skell3.gif', 'skell4.gif', 'skell5.gif', 'skell6.gif', 'skell7.gif']
+wn=trtl.Screen()
+image_number=0
+#image cycle 
+ 
+#for i in range (7):
+    #wn.addshape(gif_list[image_number])
+    #t.shape(gif_list[image_number])
+#image_number = image_number + 1 
 
 
 def change_position(): 
     t.penup()
-    t.hideturtle()
     new_xpos=rand.randint(-200,200)
     new_ypos=rand.randint(-200,200)
     t.goto(new_xpos,new_ypos)
-    t.pendown()
+    
     t.showturtle()
 
 
+def spot_clicked(x, y):
+    change_position()
+    
+   
+if spot_clicked ():
+    wn.addshape(gif_list[image_number])
+    t.shape(gif_list[image_number])
+image_number = image_number + 1 
+    
 
 
 
-turtle_clicked()
 
-trtl.onclick(turtle_clicked)
 
+
+t.onclick(spot_clicked)
+wn=trtl.Screen()
 wn.mainloop()
-
-
+#wn.mainloop()
 
 
 
