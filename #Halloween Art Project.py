a0eb61deeed31c1134de23ef0198978722e3ea14
HanGeorge <91705358+HanGeorge@users.noreply.github.com>
2021-11-03 09:46:21 -0700

    Add files via upload

--- #Halloween Art Project.py
+++ #Halloween Art Project.py
@@ -1,39 +1,45 @@
 import turtle as trtl
 import random as rand
-
-
+import time 
 
 t=trtl.Turtle()
 
-gif_list = [ 'skell2.gif', 'skell3.gif', 'skell4.gif', 'skell5.gif', 'skell6.gif', 'skell7.gif']
+gif_list = ['skell2.gif', 'skell3.gif', 'skell4.gif', 'skell5.gif', 'skell6.gif', 'skell7.gif']
 wn=trtl.Screen()
-image_number=0
-#image cycle 
- 
-#for i in range (7):
-    #wn.addshape(gif_list[image_number])
-    #t.shape(gif_list[image_number])
-#image_number = image_number + 1 
-
+wn.setup(width=1920, height=1080)
+wn.bgpic("Cemetary1.gif")
+    
+image_number = 0
+def cycle_images ():
+   
+    image_number = 0
+    while image_number<6:
+        wn.addshape(gif_list[image_number])
+        t.shape(gif_list[image_number])
+        image_number = image_number + 1 
+        time.sleep(.5)
 
 def change_position(): 
     t.penup()
     new_xpos=rand.randint(-200,200)
     new_ypos=rand.randint(-200,200)
     t.goto(new_xpos,new_ypos)
-    
     t.showturtle()
 
+def spot_clicked(x,y):
+    while image_number<6:
+        change_position()
+        cycle_images()
+    
 
-def spot_clicked(x, y):
-    change_position()
-    
-   
-if spot_clicked ():
-    wn.addshape(gif_list[image_number])
-    t.shape(gif_list[image_number])
-image_number = image_number + 1 
-    
+     
+
+
+
+
+
+t.onclick(spot_clicked)
+wn.mainloop()
 
 
 
